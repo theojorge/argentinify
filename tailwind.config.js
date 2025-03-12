bc0b9482e98e008e267a34f0d3fd292d3c177800
theo jorge <theojorge2001@gmail.com>
2025-03-12 01:41:47 -0300

    Inicializo proyecto setup con TypeScript, y TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  theme: {
+    fontFamily: {
+      sans: "Nunito, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
